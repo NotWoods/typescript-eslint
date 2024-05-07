a020974dc61ed29687784070d8a1c9a467602fa7
Tiger Oakes <contact@tigeroakes.com>
2024-05-06 20:48:06 -0700

    Migrate eslint-plugin-internal

---
+++ packages/eslint-plugin-internal/vitest.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import { defineConfig, mergeConfig } from 'vitest/config';
+import baseConfig from '../../vitest.config.base.mjs';
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      coverage: {
+        exclude: ['src/index.ts', 'src/configs/**/*.ts'],
+      },
+    },
+  }),
+);
