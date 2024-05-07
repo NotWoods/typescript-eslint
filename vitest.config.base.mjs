b5c70ecfbd6e3c4441bed4eda918f48a6aa23fd1
Tiger Oakes <contact@tigeroakes.com>
2024-05-06 19:48:18 -0700

    Migrate utils

---
+++ vitest.config.base.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: 'istanbul',
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      reporter: ['lcov'],
+    },
+    setupFiles: ['console-fail-test/setup.js'],
+    include: ['./tests/**/*.{test,spec}.ts'],
+  },
+});
