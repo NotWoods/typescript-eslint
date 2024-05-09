606a1c317904679122b3d5f403d7ba3890facfbf
Tiger Oakes <contact@tigeroakes.com>
2024-05-08 17:11:26 -0700

    Migrate typescript-eslint

---
+++ packages/typescript-eslint/vitest.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import { defineConfig, mergeConfig } from 'vitest/config';
+import baseConfig from '../../vitest.config.base.mjs';
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      include: ['./tests/**/*.ts'],
+    },
+  }),
+);
